6868655d953cfbb4f982be49769215ccb7dd98df
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2025-03-19 18:37:07 -0600

    Added: cors

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"LifeGuard/src/core/middleware"
+	"LifeGuard/src/max30102/infraestructure"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(middleware.MiddlewareCORS())
+	infraestructure.Init(r)
+	if err := r.Run(":8081"); err != nil {
+		panic(err)
+	}
+}
