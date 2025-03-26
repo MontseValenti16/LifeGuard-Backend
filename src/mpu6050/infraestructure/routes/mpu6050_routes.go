efb6bf14c9f6d7926b8ec99e27b511454153ddf9
Rodrigo Campuzano <camcluod@gmail.com>
2025-03-25 21:12:19 -0600

    update carpetas

--- src/mpu6050/infraestructure/routes/mpu6050_routes.go
+++ src/mpu6050/infraestructure/routes/mpu6050_routes.go
@@ -1,10 +1,11 @@
 package infraestructure
 
 import (
+	"LifeGuard/src/mpu6050/infraestructure/controller"
 	"github.com/gin-gonic/gin"
 )
 
-func Mpu6050Routes(r *gin.Engine, mpu6050Service *Mpu6050Controller) {
+func Mpu6050Routes(r *gin.Engine, mpu6050Service *infraestructure.Mpu6050Controller) {
 	mpu6050Group := r.Group("/mpu6050")
 	{
 		mpu6050Group.POST("/", mpu6050Service.CreateProduct)
